d4979037fbce88e41afcafbafeae176a61f95676
Alliyankhan <64958767+Alliyankhan@users.noreply.github.com>
2020-07-05 19:43:25 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,104 @@
+# print("Hello World")
+# age = int(input('What You Like?'))
+# if age > 18 :
+#   print ('You are lucky')
+# else :
+#   print ('We are same bro')
+# species = input('Animal')
+# if species == "cat":
+#   print("Yep, it's cat.")
+# else :
+#   print ('Wrong Answer')
+# city_0 = "Karachi"
+# city_1 = "Lahore "
+# city_2 = "Islamabad"
+# city_3 = "Faisalabad"
+# city_4 = "Peshawar"
+# city_5 = "Qutta"
+# print("Welcome to " + city_0)
+# 
+# 
+# val1 = int(input('Enter the first value'))
+# val2 = int(input('Enter the second value'))
+# operator = input ('Enter operator')
+
+# if operator == '+':
+#       val = val1 + val2
+#       print (val, 'answer')
+# elif operator == '-':
+#       val = val1 - val2
+#       print (val1, 'answer')
+# elif operator == '*':
+#       val = val1 * val2
+#       print (val, 'answer')
+# elif operator == '/':
+#       val = val1 / val2
+#       print (val, 'answer')
+# else :
+#       print ('Enter Correct Operator')
+  
+# first_names = ["BlueRay ", "Upchuck ", "Lojack ",
+# "Gizmo ", "Do-Rag "]
+# last_names = ["Zzz", "Burp", "Dogbone", "Droop"]
+# full_names = []
+# for a_first_name in first_names:
+#   for a_last_name in last_names:
+#    full_names.append(a_first_name + " " +
+#    a_last_name)
+#    print (full_names)
+
+# name = 'Junaid'
+# age = str(20)
+# job = 'Graphic Designer'
+# print ('{} is a well experienced person. He is {} year old. He is a Professional {}').format("name, age, job" )
+# print (name + ' is a well experienced person. He is '+ age + ' year old. He is a Professional ' + job )
+
+# customer = {"first name": "David",
+# "last name": "Elliott",
+# "address": "4803 Wellesley St."}
+# print(customer["address"])
+
+# print ("this is \\\\ double backlash")
+# print ("this is /\\/\\/\\/\\/\\ mountain")
+# print ("this is \t awesome")
+# print (" \\\" \\n \\t \\\" ")
+# print ("\U0001F603\U0001F603")
+# print ("😌😌😈")
+
+# name = "Ak"
+# age = str(20)
+# print ("{} is a best python developer in {} age".format (name, age))
+# print (f"{name} is a best python developer in {age} age")
+
+# num1 = int(input("Enter your first number" ))
+# num2 = int(input("Enter your 2 number" ))
+# num3 = int(input("Enter your 3 number" ))
+# num1, num2, num3 = input("Enter 3 numbers seprated by comma : ").split(",")
+# print (f"average of three numbers : {(int(num1) + int(num2) + int(num3)) // 3}")
+
+# print ("1 \n12 \n123 \n1234 \n12345 \n1234 \n123 \n12 \n1 ")
+
+# name=input("enter your full name: ")
+# print(f"reverse of your name is {name[-1::-1]}")
+
+# name, character = input("Enter your name and character").split(',')
+# print (f"length of your name is {len(name)}")
+# print (f"chracter count : {name.count(character)}")
+
+# name = "   Alliyan Khan    "
+# dots = "............."
+# print (name + dots)
+# print (name.lstrip() + dots)
+# print (name.rstrip() + dots)
+# print (name.strip() + dots)
+# print (name.replace( " ", ""))
+
+
+
+
+
+
+
+
+
+
